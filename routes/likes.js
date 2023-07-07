569e3fc7f57e2df8639b2acd9d3877e642ab990b
Kim Joohee <syap1170@gmail.com>
2023-07-08 00:37:52 +0900

    feat: 좋아요 수 추가, 삭제

--- routes/likes.js
+++ routes/likes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const authMiddleware = require('../middlewares/auth-middleware.js');
-const { Posts, Likes } = require('../models');
+const { Users, Posts, Likes } = require('../models');
 
-// 좋아요 API
+// 게시글 좋아요 API
 router.post('/posts/:postId/like', authMiddleware, async (req, res) => {
   const { postId } = req.params;
   const userId = res.locals.user;
@@ -17,17 +17,25 @@ router.post('/posts/:postId/like', authMiddleware, async (req, res) => {
       .json({ errorMessage: '존재하지 않는 게시글입니다.' });
   }
 
-  const likes = await Likes.findOne({
+  const like = await Likes.findOne({
     where: { UserId: userId, PostId: post.postId },
   });
 
-  if (!likes) {
+  if (!like) {
+    await Posts.update(
+      { likes: post.likes + 1 },
+      { where: { postId: post.postId } },
+    );
     await Likes.create({ UserId: userId, PostId: post.postId });
     return res.status(200).json({ message: '게시물에 좋아요를 눌렀습니다.' });
-  } else {
-    await Likes.destroy({ where: { UserId: userId, PostId: post.postId } });
-    return res.status(200).json({ message: '좋아요를 취소했습니다.' });
   }
+
+  await Posts.update(
+    { likes: post.likes - 1 },
+    { where: { postId: post.postId } },
+  );
+  await Likes.destroy({ where: { UserId: userId, PostId: post.postId } });
+  return res.status(200).json({ message: '좋아요를 취소했습니다.' });
 });
 
 module.exports = router;
